332e6e6557a68393d80b0b9a4dce213d8c24deca
GabrielBalestra <146239343+GabrielBalestra@users.noreply.github.com>
2025-08-04 11:53:15 -0300

    .

--- script.js
+++ script.js
@@ -1,2 +1,20 @@
 const caixaPrincipal = document.querySlector(".caixa-principal");
-const 
+ 
+const caixaPergutas = document.querySlector(".caixa-Pergutas");
+
+const caixaAlternativas = document.querySlector(".caixa- alternativas");
+
+const caixaResultado = document.querySlector(".caixa-resultado");
+
+const caixaResultado = document.querySlector(".texto-resultado");
+
+const lista = []
+
+const pergunta = ]
+{
+    enuciado:"pergunta 1",
+    alternativa: [
+        "alternativa 1", 
+        "alternativa 2", 
+    ]
+}
